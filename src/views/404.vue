d7fe2ab333b1754fc97fc1c905f82c0b78415812
M Atif <chatif476@gmail.com>
2022-03-27 21:25:31 +0500

    updated 404 page

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,4 +1,9 @@
-<template>404</template>
+<template>
+  <section class="flex flex-col justify-center items-center h-96">
+    <div class="text-white text-7xl font-bold">404</div>
+    <p class="text-white">Page Not Found</p>
+  </section>
+</template>
 
 <script lang="ts" setup></script>
 
